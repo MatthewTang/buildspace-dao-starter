f24f52858f4e2d77634f660d35b0eb5332bc2781
Matthew Tang <matthew.tang@connect.hku.hk>
2021-12-25 02:42:39 +0800

    Add deploy-drop script

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,25 @@
+import { useEffect, useMemo, useState } from "react";
+
+// import thirdweb
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+
+  // Use the connectWallet hook thirdweb gives us.
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("👋 Address:", address)
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to sandwichDAO 🥪</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="landing">
       <h1>Welcome to My DAO</h1>
